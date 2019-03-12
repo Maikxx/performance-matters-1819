aa5c59dae4aeae8e013eb3d3f4f73718ddbbaaa0
Maikel van Veen <maikeloo10@hotmail.com>
2019-03-12 16:52:16 +0100

    feat(*): add semi-working code in the detail , missing linkthroughs

--- server/src/helpers/getCharacters.ts
+++ server/src/helpers/getCharacters.ts
@@ -4,18 +4,24 @@ import { Transformer } from '../utils/Transformer'
 import { Filter } from '../utils/Filter'
 import { Sorter } from '../utils/Sorter'
 
-export async function getCharacters() {
+export async function getCharacters(byId?: string) {
     const baseUrl = `https://anapioficeandfire.com/api/characters`
-    const charactersUrl = `${baseUrl}/?pageSize=${50}&page=${1}`
+    const fullUrl = byId
+        ? `${baseUrl}/${byId}`
+        : `${baseUrl}/?pageSize=${50}&page=${1}`
 
     try {
-        const res = await fetch(charactersUrl)
+        const res = await fetch(fullUrl)
         const data: Character[] = await res.json()
-        const cleanData: Character[] = Transformer.deepCleanArray(data)
+
+        const cleanData: Character[] = byId
+            ? [Transformer.deepCleanObject(data)]
+            : Transformer.deepCleanArray(data)
         const uniqueCharacters = Filter.getUniqueArrayByObjectKey<Character>(cleanData, 'name')
         const transformedData = uniqueCharacters
             .filter(character => !!character.name)
             .map(character => ({
+                ...character,
                 name: Transformer.capitalize(character.name),
                 url: `/characters/${character.url.slice(character.url.lastIndexOf('/') + 1)}`,
             }))
--- server/src/index.ts
+++ server/src/index.ts
@@ -10,12 +10,33 @@ import { getCharacters } from './helpers/getCharacters'
 
     app.set('view engine', 'ejs')
     app.set('views', `${__dirname}/views`)
+
     app.get('/', async (request: Express.Request, response: Express.Response) => {
         const characters = await getCharacters()
 
         response.status(200).render('pages/index', {
             characters,
         })
+    })
+
+    app.get('/characters/:id', async (request: Express.Request, response: Express.Response) => {
+        const { id } = request.params
+        const character = await getCharacters(id)
+        const allowedKeys = [ 'url', 'id' ]
+        const characterData = Object.keys(character['0'])
+            .filter(key => !allowedKeys.includes(key))
+            .reduce((obj, key) => {
+                obj[key] = character['0'][key]
+                return obj
+            }, {})
+
+        response.status(200).render('pages/characterDetail', {
+            character: characterData,
+        })
+    })
+
+    app.get('*', (request: Express.Request, response: Express.Response) => {
+        response.status(404).send('This page does not exist!')
     })
 
     app.listen(({ port: 3000 }), () => {
