e19d2462033c957f6aae95f57b2eb81f47dfc7f7
Maikel van Veen <maikeloo10@hotmail.com>
2019-03-14 09:38:18 +0100

    feat(*): add working build process for css

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -2,7 +2,9 @@ const gulp = require('gulp')
 const cleanCSS = require('gulp-clean-css')
 
 gulp.task('minifyCss', () => {
-    return gulp.src('styles/*.css')
+    return gulp.src('src/css/*.css')
         .pipe(cleanCSS())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('public/css'))
 })
+
+gulp.watch(['src/css/*.css'], gulp.series('minifyCss'))
