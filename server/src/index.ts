298893ea11b293efc81c59ef15ffb3b7771a957f
Maikel van Veen <maikeloo10@hotmail.com>
2019-03-14 13:01:14 +0100

    feat(*): add memory caching

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import bodyParser from 'body-parser'
 import { getIndexRoute, getCharacterDetailRoute, getBookDetailRoute, getHouseDetailRoute, getErrorRoute } from './routes/getRoutes'
 import { getMemoryData } from './helpers/getMemoryData'
 import { postSearchRoute } from './routes/postRoutes'
+import { cache } from './helpers/memoryCache'
 
 (async() => {
     const app = Express()
@@ -17,13 +18,14 @@ import { postSearchRoute } from './routes/postRoutes'
     app.set('views', `${__dirname}/views`)
 
     const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
     const { characters, books, houses } = await getMemoryData()
 
-    app.get('/', getIndexRoute(characters))
-    app.get('/characters/:id', getCharacterDetailRoute(characters))
-    app.get('/books/:id', getBookDetailRoute(books))
-    app.get('/houses/:id', getHouseDetailRoute(houses))
-    app.get('*', getErrorRoute)
+    app.get('/', cache(50), getIndexRoute(characters))
+    app.get('/characters/:id', cache(50), getCharacterDetailRoute(characters))
+    app.get('/books/:id', cache(50), getBookDetailRoute(books))
+    app.get('/houses/:id', cache(50), getHouseDetailRoute(houses))
+    app.get('*', cache(50), getErrorRoute)
 
     app.post('/search', urlencodedParser, postSearchRoute)
 
