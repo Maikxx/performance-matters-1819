b94e01f5eddd0e45966d132e7c9a29f3c2eebae4
Maikel van Veen <maikeloo10@hotmail.com>
2019-03-14 09:55:56 +0100

    feat(autprefixer): add autoprefixer

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,9 +1,18 @@
 const gulp = require('gulp')
 const cleanCSS = require('gulp-clean-css')
+const autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('minifyCss', () => {
     return gulp.src('src/css/*.css')
-        .pipe(cleanCSS())
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({
+            browsers: [
+                'last 2 versions',
+                '> 1%',
+                'maintained node versions',
+                'not dead'
+            ],
+        }))
         .pipe(gulp.dest('public/css'))
 })
 
