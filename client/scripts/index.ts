dc4b8abc52646aac0ccd07f70ef4a24840b94e9e
Maikel van Veen <maikeloo10@hotmail.com>
2019-03-19 14:47:57 +0100

    feat(search): add client side search

--- client/scripts/index.ts
+++ client/scripts/index.ts
@@ -1,8 +1,41 @@
 const searchForm = document.querySelector('.SearchForm')
-// searchForm.addEventListener('submit', event => {
-//     event.preventDefault()
+searchForm.addEventListener('submit', onSubmit)
 
-//     const { hostname, port } = window.location
-//     const url = `${hostname}:${port}/search`
+async function onSubmit(event: Event) {
+    event.preventDefault()
 
-// })
+    const { target } = event
+    if (!target) {
+        return
+    }
+
+    const input = (target as HTMLElement).querySelector('input')
+
+    if (!input) {
+        return
+    }
+
+    const { value } = input
+    const { protocol, hostname, port } = window.location
+    const url = `${protocol}//${hostname}:${port}/search?searchText=${value}`
+    const response = await fetch(url, {
+        method: 'POST',
+    })
+    const { characters } = await response.json()
+    const infiniteScrollList = document.querySelector('.InfiniteScrollList')
+
+    if (!infiniteScrollList || !characters || characters.length === 0) {
+        return window.location.reload()
+    }
+
+    const htmlString = characters.reduce((content: string, character: any) => {
+        return content += `
+            <li>
+                <a class="CharacterButton" href=${character.detailUrl}>
+                    ${character.name}
+                </a>
+            </li>`
+    }, '')
+
+    infiniteScrollList.innerHTML = htmlString
+}
